97e0091433c64e2139b2575cd20019fef88157bf
NiteshMourya009 <niteshmourya009@gmail.com>
2024-07-25 09:48:52 +0530

    Merge Sort

---
+++ MergeSort.java
@@ -0,0 +1,56 @@
+public class MergeSort {
+    public static void Merge(int[]arr, int l, int m, int r){
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        for (int i = 0; i < n1; ++i){
+            L[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; ++j){
+            R[j] = arr[m + 1 + j];
+        }
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void Sort(int[]nums, int l, int r){
+        if(l<r){
+            int m= l + (r-l) / 2 ;
+            Sort(nums,l,m);
+            Sort(nums, m+1, r);
+
+            Merge(nums, l, m, r);
+
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {4, 6, 8, 2, 1, 3, 9};
+        Sort(nums, 0, nums.length-1);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] +" ");
+        }
+    }
+}
